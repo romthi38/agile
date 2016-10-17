b2d52a20b3eb9847e757a48ada8db4f4a523489d
lucien-chastan <chastanlucien@gmail.com>
2016-10-17 15:29:03 +0200

    debug interface

--- interface.js
+++ interface.js
@@ -1,5 +1,15 @@
 var resultat = null, nombre_courant = document.getElementById('resultat');
-var operateur = document.getElementsByClassName('operation');
+var operateur = document.getElementsByClassName('operation'), bt_number = document.getElementsByClassName('valeur');
+
+window.onload = function(){
+    for(var i = 0 ; i < operateur.lenght ; i++){
+        operateur[i].addEventListener('click', click_operation);
+    }
+    
+    for(var i = 0 ; i < bt_number.lenght ; i++){
+        bt_number[i].addEventListener('click', click_number);
+    }
+}
 
 window.onload = function(){
     var histo = document.getElementById("historique"); //fonction permettant d'ajouter à l'historique
@@ -10,24 +20,31 @@ window.onload = function(){
 function click_number(){
         var res = document.getElementById("resultat");
         res = res + button.value;
-    } 
+    }  
 }
 
-operateur.onclick = function(){
+function click_operation(){
     switch(this.value)
         {
         
-            case "+"
-                    resultat = addition(resultat, nombre_courant);
-                    break;
-            case "-"
-                    resultat = soustraction(resultat, nombre_courant);
-                    break;
-            case "x"
-                    resultat = multiplier(resultat, nombre_courant);
-                    break;
-            case "/"
-                    resultat = diviser(resultat, nombre_courant);
-                    break;
+            case "+":
+                resultat = addition(resultat, nombre_courant);
+                break;
+            case "-":
+                resultat = soustraction(resultat, nombre_courant);
+                break;
+            case "x":
+                resultat = multiplier(resultat, nombre_courant);
+                break;
+            case "/":
+                resultat = diviser(resultat, nombre_courant);
+                break;
+            default:
+                return false;
         }
+    affiche();
+}
+
+function affiche(){
+    nombre_courant.value = resultat;
 }
